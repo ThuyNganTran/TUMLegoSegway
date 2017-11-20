e3d9bd3e80d771f90fa31aa59c04b8b59000a95b
Dao Thuy Ngan <thuy.tran@tum.de>
2017-11-20 13:43:54 +0100

    Adding boost library

--- implementation/receive.cpp
+++ implementation/receive.cpp
@@ -17,15 +17,17 @@ using namespace Tins;
 using namespace std;
 using namespace boost::asio;
 
-void handle_receive(  const boost::system::error_code& error, // Result of operation.
-                    std::size_t bytes_transferred){
+string receive_from(){
     cout << error;
 }
 
 int main(int arg, char** args){
     io_service io_service;
     ip::udp::endpoint local_endpoint_;
-    local_endpoint_ =
-    socket.open(ip::udp::v4());
+    
+    local_endpoint_ = ip::udp::endpoint(ip::address::from_string("131.159.222.111"), 63160);
+    ip::udp::socket socket(io_service);
+    
+    socket.connect(ip::udp::v4());
     socket.async_receive_from(buffer(recv_buffer, 8), local_endpoint_, handle_receive);
 }
